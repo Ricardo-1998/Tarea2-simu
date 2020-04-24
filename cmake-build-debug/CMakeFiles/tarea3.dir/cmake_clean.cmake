68a973c6d50fce9c2aabc7bcea31466730dd5762
Ricardo-1998 <00004817@uca.edu.sv>
2020-04-23 19:23:42 -0600

    primer commit

---
+++ cmake-build-debug/CMakeFiles/tarea3.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/tarea3.dir/main.cpp.obj"
+  "libtarea3.dll.a"
+  "tarea3.exe"
+  "tarea3.exe.manifest"
+  "tarea3.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/tarea3.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
